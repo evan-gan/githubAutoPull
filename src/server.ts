afb9399a37915d6789268f0b3e7b9856b4bb6a78
evan-gan <139721374+evan-gan@users.noreply.github.com>
2024-06-29 14:56:21 -0400

    Fixed limit?

--- src/server.ts
+++ src/server.ts
@@ -143,7 +143,7 @@ async function setup() {
 //Rate limits! 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 3, // limit each IP to 100 requests per windowMs
+    limit: 3, // limit each IP to 100 requests per windowMs
     message: 'Too many requests from this IP, please try again later.',
     handler: (req, res) => {
         logWarn(`Rate limit exceeded for IP ${req.ip}`);
